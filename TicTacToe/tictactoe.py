dea272acc5879ffc044b2abfabee4a4222bb817c
Dima Gonikman <dima.gonikman@gmail.com>
2017-04-28 22:22:11 +0300

    Draw end game option

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,5 +1,3 @@
-print 'Welcome to Tic-Tac-Toe!'
-
 # Global Init
 board = [[1,2,3],[4,5,6],[7,8,9]]
 turns = 0
@@ -8,9 +6,20 @@ playerIndex = 0
 def HasWinner(): # Evaluate board for a winner
     global turns
     turns += 1
-    if turns > 4:
+    if turns > 10:
+        print 'Game Over, {winner} is the winner!'.format(winner=players[playerIndex])
         return True
     return False
+
+def EndGame():
+    global turns
+    if turns == 10:
+        print "Game Over, it's a draw."
+        return True
+    return False
+
+# Start Game
+print 'Welcome to Tic-Tac-Toe!'
 
 # Get players' names 
 player1 = raw_input("Player1, what's your name? ") or "Player1"
@@ -18,12 +27,10 @@ player2 = raw_input("Player2, what's your name? ") or "Player2"
 players = (player1, player2)
 
 # Play
-print "{x}\n{y}\n{z}".format(x=board[0], y=board[1], z=board[2])
-while not HasWinner():
+while not HasWinner() and not EndGame():
+    print "{x}\n{y}\n{z}".format(x=board[0], y=board[1], z=board[2])
     print "{p}, it's your turn.".format(p=players[playerIndex])
     # Get input from current player
     # Check board
     playerIndex += 1
     playerIndex %= 2
-    
-print 'Game Over, {winner} is the winner!'.format(winner=players[playerIndex])

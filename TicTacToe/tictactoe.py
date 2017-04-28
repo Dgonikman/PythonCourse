e2b7c6848441f31ee45e6b00046ba30100ade2ed
Dima Gonikman <dima.gonikman@gmail.com>
2017-04-28 23:08:15 +0300

    Renamed methods to python convention

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -4,43 +4,43 @@ marks = ('X', 'O')
 turns = 0
 playerIndex = 0
 
-def HasWinner(): # Evaluate board for a winner
+def has_winner(): # Evaluate board for a winner
     global turns
     turns += 1
 
-    if CheckRows(board):
+    if check_rows(board):
         return True
-    if CheckRows(zip(*board)):
+    if check_rows(zip(*board)):
         return True
     if board[0][0] == board[1][1] == board[2][2] or board[0][2] == board[1][1] == board[2][0]:
         mark = board[1][1]
-        DeclareWinner(marks.index(mark))
+        declare_winner(marks.index(mark))
         return True
     return False
 
-def CheckRows(matrix):
+def check_rows(matrix):
     for row in matrix:
         rowSet = set(row)
         if len(rowSet) == 1:
             mark = rowSet.pop()
-            DeclareWinner(marks.index(mark))
+            declare_winner(marks.index(mark))
             return True
 
-def DeclareWinner(winnerIdx):
+def declare_winner(winnerIdx):
     print 'Game Over, {winner} is the winner!'.format(winner=players[winnerIdx])
 
-def EndGame():
+def end_game():
     global turns
     if turns == 10:
         print "Game Over, it's a draw."
         return True
     return False
 
-def PrintBoard():
+def print_board():
     global board
     print "{x}\n{y}\n{z}".format(x=board[0], y=board[1], z=board[2])
 
-def MarkBoard(cell):
+def mark_board(cell):
     for row in board:
         if cell in row:
             idx = row.index(cell)
@@ -55,13 +55,13 @@ player2 = raw_input("Player2, what's your name? ") or "Player2"
 players = (player1, player2)
 
 # Play
-PrintBoard()
-while not HasWinner() and not EndGame():
+print_board()
+while not has_winner() and not end_game():
     # Get input from current player
     print "{p}, it's your turn.".format(p=players[playerIndex])
     cell = int(raw_input("Choose cell: "))
-    MarkBoard(cell)
-    PrintBoard()
+    mark_board(cell)
+    print_board()
     # Check board
     playerIndex += 1
     playerIndex %= 2

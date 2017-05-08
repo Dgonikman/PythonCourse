29a56c80d5505f3ea18f85dc494b5c7d6786bec4
Dima Gonikman <dima.gonikman@gmail.com>
2017-05-08 08:10:49 +0300

    Added dealers' hand play

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -10,6 +10,30 @@ MINIMAL_BET = 1
 dealer_hand = Hand(Player("Dealer", None), None)
 active_players = [Player("Player1", INITIAL_BALANCE)]
 shoe = Shoe(6)
+def play_hand(hand_to_play, action):
+    player_name = hand_to_play.Player.Name
+    if action == 'S' or action == 's':
+        print player_name + " chose to stand"
+        hand_to_play.Status = State.Stand
+    elif action == 'H' or action == 'h':
+        print player_name + " chose to hit"
+        hand_to_play.deal(shoe.pop())
+        print hand_to_play
+        if hand_to_play.value == 21:
+            print "21! " + player_name + ", your'e done"
+            hand_to_play.Status = State.Stand
+        elif hand_to_play.value > 21:
+            print str(hand_to_play.value) + ". Your hand is dead, " + player_name
+            hand_to_play.Status = State.Bust
+    elif action == 'D' or action == 'd':
+        print "Not supported yet."
+    else:
+        print "Illegal move, try again."
+
+
+def any_stand_hands():
+    stand_hands = [hand for hand in finished_hands if hand.Status == State.Stand]
+    return len(stand_hands) > 0
 
 
 def print_hand():
@@ -46,21 +70,7 @@ while len(active_players) > 0:
 
         while playing_hand.Status == State.Active:
             print_hand()
-            action = raw_input("(S)tand\(H)it: ")
-            if action == 'S' or action == 's':
-                print "You chose to stand"
-                playing_hand.Status = State.Stand
-            elif action == 'H' or action == 'h':
-                print "You chose to hit"
-                playing_hand.deal(shoe.pop())
-                if playing_hand.value == 21:
-                    print "21! Your'e done"
-                    playing_hand.Status = State.Stand
-                elif playing_hand.value > 21:
-                    print str(playing_hand.value) + ". Your hand is dead, sir"
-                    playing_hand.Status = State.Bust
-            else:
-                print "Illegal move, try again."
+            play_hand(playing_hand, raw_input("(S)tand\(H)it\(D)ouble: "))
 
         finished_hands.append(playing_hand)
 
@@ -69,9 +79,19 @@ while len(active_players) > 0:
     print
     print dealer_hand
 
+    while dealer_hand.Status == State.Active and any_stand_hands():
+        while dealer_hand.value < 16:
+            raw_input("Hit any key to deal next card...")
+            play_hand(dealer_hand, 'H')
+        if dealer_hand.Status == State.Active:
+            dealer_hand.Status = State.Stand
+        print
+        print dealer_hand
+
     # Pay
+    # Bankrupt everybody
     for player in active_players:
-        player.Balance -= player.Balance
+        # player.Balance -= player.Balance
 
     # Check remaining cash
     bankrupt_players = [player for player in active_players if player.Balance <= 0]
@@ -80,5 +100,6 @@ while len(active_players) > 0:
     print
     for player in bankrupt_players:
         print player.Name + " is bankrupt!"
+    raw_input("Hit any key for the next round...")
 
 print "Game over! House always wins!"
